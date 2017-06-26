a69429c450e2f2bcd6fb05bc57720d395feb1037
EkaterinaAidova <aidova.ekaterina@gmail.com>
2017-06-26 17:16:13 +0300

    интерфейсы для бизес-логики

---
+++ Models/Services/Contracts/IWorkWithLogsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+namespace BusinessLogicLayer.Services.Contracts
+{
+    public interface IWorkWithLogsService
+    {
+        void addNewLog()
+        {
+ 
+        }
+    }
+}
